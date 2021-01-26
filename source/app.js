14baedee8b75ea08c7d68b1d527c4197d5f58269
unknown <Administrator@COPITEC.LOCAL>
2021-01-26 11:29:44 -0300

    First commit

---
+++ source/app.js
@@ -0,0 +1,44 @@
+const express = require('express'),
+      morgan = require('morgan'),
+      path = require('path'),
+      mysql = require('mysql');
+const app = express();
+
+var matriculadoRouter = require('./routes/matriculado');
+
+                    //Settings            -------------------------
+
+app.set('port', 3000);
+app.set('view engine', 'pug');
+app.set('views', path.resolve(__dirname, 'views'));
+
+                    //Middlewares         -------------------------
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+
+//Validate dynamic route of matriculado
+matriculadoRouter.param('nroMatricula', (req, res, next, nroMatricula)=>{
+  if(nroMatricula == ''){
+    console.log('Error No introdujo un nro de matricula');
+  }else{
+    req.nroMatricula = nroMatricula;
+    next()
+  }
+});
+
+                    //Routes              -------------------------
+
+app.use(require('./routes/index.js'));
+app.use('/matriculado', matriculadoRouter);
+
+                    //Statics             -------------------------
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+                   //404 handle
+app.use((req, res, next)=>{
+  res.status(404).send('404 Page no found');
+});
+
+module.exports = app;
---
+++ source/routes/index.js
@@ -0,0 +1,9 @@
+const { Router } = require('express');
+const router = Router();
+
+//Router test
+router.get('/', (req, res)=>{
+  res.render('index');
+});
+
+module.exports = router;
