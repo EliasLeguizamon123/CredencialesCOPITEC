3feaa0bd492ca9110edf95d9c480be38dba41516
Elías Leguizamón <elias.leguizamon1997@gmail.com>
2021-05-05 10:33:03 -0300

    Add files via upload

--- source/app.js
+++ source/app.js
@@ -1,32 +1,26 @@
-const express = require('express'),
-      morgan = require('morgan'),
-      path = require('path'),
-      mysql = require('mysql');
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+require('dotenv').config({ path: './.env' })
+
 const app = express();
 
 var matriculadoRouter = require('./routes/matriculado');
 
                     //Settings            -------------------------
 
-app.set('port', 3000);
+app.set('port', process.env.PORT);
   //Need change pug for React
 app.set('view engine', 'pug');
 app.set('views', path.resolve(__dirname, 'views'));
+require('./database/database.js')
 
                     //Middlewares         -------------------------
 
 app.use(morgan('dev'));
+app.use(express.json())
 app.use(express.urlencoded({extended: false}));
-
-//Validate dynamic route of matriculado
-matriculadoRouter.param('nroMatricula', (req, res, next, nroMatricula)=>{
-  if(nroMatricula == ''){
-    console.log('Error No introdujo un nro de matricula');
-  }else{
-    req.nroMatricula = nroMatricula;
-    next()
-  }
-});
 
                     //Routes              -------------------------
 
@@ -39,7 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
                    //404 handle
 app.use((req, res, next)=>{
-  res.status(404).send('404 Page no found');
+  res.status(404).send('404 Page not found');
 });
 
 module.exports = app;
